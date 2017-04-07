8b3d65df49d08bddfdec9b9642680e4a17355cdb
Fayez O Saadi <fayezosaadi@gmail.com>
2017-04-07 00:13:39 +0000

    Map Function

--- map.js
+++ map.js
@@ -1,20 +1,16 @@
-var words = ["ground", "control", "to", "major", "tom"];
+  var words = ["ground", "control", "to", "major", "tom"];
 
-// map(words, function(word) {
-//   return word.length;
-// });
-
-function map (words, cb){
-  var newArray = [];
-  for (var i = 0; i < words.length; i++){
-    var tempresult = cb (words[i]);
-    newArray.push(tempresult);
+  function map (words, cb){
+    var newArray = [];
+    for (var i = 0; i < words.length; i++){
+      var tempresult = cb (words[i]);
+      newArray.push(tempresult);
+    }
+    return newArray;
   }
-  return newArray;
-}
 
-function wordLength (word) {
-  return word.length;
-}
+  function wordLength (word) {
+    return word.length;
+  }
 
-console.log(map(words, wordLength));
+  console.log(map(words, wordLength));
